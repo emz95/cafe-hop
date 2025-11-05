a44688c0e0be61051a0f4ba6a9171821717f753a
emz95 <emma91zhang@gmail.com>
2025-11-04 22:20:39 -0800

    Add route to get user by ID

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,6 +19,20 @@ router.get('/', async (_req, res) => {
     res.json(users);
 });
 
+//get by userID
+router.get('/:id', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id).lean()
+        if(!user) {
+            return res.status(404).json({error: 'User not found'})
+        }
+        res.json(user)
+
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
+})
+
 
 
 module.exports = router;

e44d36bed334fcce713701178bebba539d0f697c
Grace Y <grace.shaw.yan@gmail.com>
2025-11-14 20:38:10 -0800

    Add ReviewCard component for cafe reviews

---
+++ src/components/Button.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ReviewCard = ({ review }) => {
+  const renderTeaRating = (rating) => {
+    return '🍵'.repeat(rating) + '⚪'.repeat(5 - rating);
+  };
+
+  return (
+    <div className="review-card">
+      <div className="review-header">
+        <div className="review-user-info">
+          <div className="profile-picture profile-picture-small">
+            <div className="profile-placeholder">{review.username.charAt(0).toUpperCase()}</div>
+          </div>
+          <div>
+            <h4 className="review-username">{review.username}</h4>
+            <p className="review-date">{review.date}</p>
+          </div>
+        </div>
+        <div className="review-rating-display">
+          {renderTeaRating(review.rating)}
+        </div>
+      </div>
+      <p className="review-text">{review.text}</p>
+      {review.images && review.images.length > 0 && (
+        <div className="review-images">
+          {review.images.map((img, idx) => (
+            <img key={idx} src={img} alt={`Review ${idx + 1}`} className="review-image" />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ReviewCard;

1d1338284da9d9bb58d31cc2ff0f63f6d6e42c66
emz95 <emma91zhang@gmail.com>
2025-11-16 20:20:22 -0800

    Merge edits

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -18,33 +18,6 @@ router.delete('/:id', protect, deletePost)
 router.get('/user/:id', getUserPosts )
 router.get('/leaderboard', getLeaderboard)
 
-<<<<<<< HEAD
-=======
-router.get('/', async (_req, res) => {
-    const posts = await Post.find().lean();
-    res.json(posts);
-
-});
-
-router.get('/:id', async (req, res) => {
-    try {
-        const id = req.params.id; 
-        const post = await Post.findById(id).populate('author').lean();
-        if(!post) {
-            res.status(404).json({error: "post not found"});
-            return;
-        }   
-        res.json(post);
-    }
-    catch (err) {
-        res.status(400).json({error: err.message});
-        
-    }
-
-
-
-});
->>>>>>> 70a5c9d (made cafe review and cafe controller)
 
 module.exports = router; 
 

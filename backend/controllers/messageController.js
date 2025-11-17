b3915876639c9e8788359dc63717263f570fd8c5
riverkngo <riverkngo@gmail.com>
2025-11-17 14:55:24 -0800

    added middleware controller notation to messages router

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require('express-async-handler');
+
+const Message = require('../models/Message');
+
+const createMessage = asyncHandler(async (req, res) => {
+    const message = await Message.create({
+        sender: req.user._id,
+        group: req.body.group,
+        text: req.body.text,
+    });
+    res.status(201).json(message); 
+
+})
+
+const getMessagesByGroupChat = asyncHandler(async (req, res) => {
+    const messages = await Message.find({group: req.params.groupChatId}).
+    sort({createdAt: -1}).
+    populate('sender', '_id username').
+    lean(); 
+    res.json(messages); 
+
+
+});
+
+module.exports = {
+    createMessage,
+    getMessagesByGroupChat
+};
--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,32 +1,14 @@
 const express = require('express');
 
-const Message = require('../models/Message');
+const { createMessage, getMessagesByGroupChat } = require('../controllers/messageController');
+
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-        const message = await Message.create(req.body);
-        res.status(201).json(message);
-    }
-    catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+router.post('/', protect, createMessage);
 
-router.get('/:GroupChatId', async (req, res) => {
-    try {
-        const messages = await Message.find({group: req.params.GroupChatId})
-        .sort({createdAt: 1})
-        .populate('sender', 'username _id')
-        .lean();
-        res.json(messages);
-    }
-    catch (error) {
-        res.status(404).json({error: "could not find gc"});
+router.get('/:groupChatId', protect, getMessagesByGroupChat);
 
-    }
-
-});
 
 module.exports = router;

4d7e9fc6256fe5bf228f778bf273db1f47fc3570
emz95 <emma91zhang@gmail.com>
2025-11-23 15:59:40 -0800

    Fix bug with create post

--- src/screens/CafeTripPostScreen.jsx
+++ src/screens/CafeTripPostScreen.jsx
@@ -31,7 +31,7 @@ const CafeTripPostScreen = () => {
           ...(token ? { Authorization: `Bearer ${token}` } : {}),
         },
         body: JSON.stringify({
-          title: cafeName,
+          cafeName: cafeName,
           description,
           date: dateTime.toISOString(),
           location,

e745008af509af32a536e2fd1c29329bb9db788e
emz95 <emma91zhang@gmail.com>
2025-11-23 16:40:36 -0800

    Connect leaderboard to backend

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -4,7 +4,6 @@ const JoinRequest = require('../models/JoinRequest');
 const User = require('../models/User');
 
 const getPosts = asyncHandler(async (req, res) => {
-    console.log("REACHED HEREEEE")
     const {
         search,
         dateGoing,
@@ -41,7 +40,7 @@ const getPosts = asyncHandler(async (req, res) => {
     console.log("Filter: ", filter )
 
     const posts = await Post.find(filter)
-        .sort({createdAt: -1})
+        .sort({date: 1})
         .populate('author', 'username')
     console.log("posts")
 
@@ -137,10 +136,7 @@ const getLeaderboard = asyncHandler( async (req, res) => {
         addCount(join.user)
     })
 
-    const userIds = Object.keys(counts)
-    if (userIds.length === 0) {
-        return res.status(200).json([])
-    }
+    const userIds = [...counts.keys()]
 
     const users = await User.find({_id: {$in: userIds}}, 'username').lean()
 
@@ -149,7 +145,7 @@ const getLeaderboard = asyncHandler( async (req, res) => {
         return {
             _id: id,
             username: user.username,
-            trips: counts[id]
+            trips: counts.get(id)
         }
     }).sort((a, b) => b.trips - a.trips)
 
